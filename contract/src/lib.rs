e14e938c193d7f891c27e107e9fcaafb7dae5159
Polpedu <emailparaoedu@gmail.com>
2022-08-03 18:41:55 +0100

    Frã és um estupido

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,3 +1,4 @@
+use near_contract_standards::non_fungible_token::core::NonFungibleTokenCore;
 /*
  * Example smart contract written in RUST
  *
@@ -5,12 +6,17 @@
  * https://near-docs.io/develop/Contract
  *
  */
-
+use near_contract_standards::non_fungible_token::metadata::{
+    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
+};
+use near_contract_standards::non_fungible_token::{Token, TokenId};
+use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::collections::LazyOption;
+use near_sdk::{
+    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+};
 
-// Define the default message
-const DEFAULT_MESSAGE: &str = "Hello";
 
 // Define the contract structure
 #[near_bindgen]
@@ -20,7 +26,10 @@ pub struct Contract {
     metadata: LazyOption<NFTContractMetadata>,
 }
 
+// svg da jeknowledge
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+
+const INITIAL_BALANCE: Balance = 250_000_000_000_000_000_000_000; // 2.5e23yN, 0.25N
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -30,6 +39,22 @@ enum StorageKey {
     Enumeration,
     Approval,
 }
+
+impl Default for Contract {
+    fn default() -> Self {
+      Self  {
+        tokens: NonFungibleToken::new(
+            StorageKey::NonFungibleToken,
+            owner_id,
+            Some(StorageKey::TokenMetadata),
+            Some(StorageKey::Enumeration),
+            Some(StorageKey::Approval),
+        ),
+        metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+    }
+  
+    }
+  }
 
 #[near_bindgen]
 impl Contract {
@@ -41,8 +66,8 @@ impl Contract {
             owner_id,
             NFTContractMetadata {
                 spec: NFT_METADATA_SPEC.to_string(),
-                name: "Example NEAR non-fungible token".to_string(),
-                symbol: "EXAMPLE".to_string(),
+                name: "jeKnowledge NFT".to_string(),
+                symbol: "jeK".to_string(),
                 icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
                 base_uri: None,
                 reference: None,
@@ -51,11 +76,11 @@ impl Contract {
         )
     }
 
-    #[init]
+    #[private]
     pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
-        Self {
+        let mut contract = Self {
             tokens: NonFungibleToken::new(
                 StorageKey::NonFungibleToken,
                 owner_id,
@@ -64,19 +89,51 @@ impl Contract {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
-        }
+        };
+        contract
     }
 
 
-    #[payable]
+    #[private]
+    pub fn create_subaccount(prefix: AccountId) -> Promise {
+        let subaccount_id = AccountId::new_unchecked(
+            // prefix.jeknowledge.testnet
+          format!("{}.{}", prefix, env::current_account_id())
+        );
+
+        Promise::new(subaccount_id)
+            .create_account()
+            .add_full_access_key(env::signer_account_pk())
+            .transfer(INITIAL_BALANCE)
+    }
+    
+    #[warn(deprecated)]
     pub fn nft_mint(
         &mut self,
         token_id: TokenId,
         receiver_id: AccountId,
         token_metadata: TokenMetadata,
     ) -> Token {
+        // assert that the signer is the owner of the contract
+        assert_eq!(env::signer_account_id(), env::current_account_id(), "Only the owner can mint tokens");
+
+        create_subaccount(receiver_id);
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
     }
+
+
+    pub fn nft_transfer(
+        &mut self,
+        token_id: TokenId,
+        receiver_id: AccountId,
+    ) -> Token {
+        // assert that the signer is the owner of the contract
+        assert_eq!(env::signer_account_id(),env::current_account_id(), "Only the owner can transfer");
+
+        self.tokens.nft_transfer(receiver_id, token_id, None, "Francisco Gaspar é o meu idolo.");
+    }
+
+    
 }
 
 #[near_bindgen]
@@ -118,7 +175,7 @@ mod tests {
             media_hash: None,
             copies: Some(1u64),
             issued_at: None,
-            expires_at: None,
+            expires_at: None,pçl+'«
             starts_at: None,
             updated_at: None,
             extra: None,
